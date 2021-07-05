4acd4246116a0678c782c2b0fc9a371b8d851f8c
jccsISC <silva.jccs18@gmail.com>
2021-07-05 11:46:26 +0200

    Generando apk sdk en java

--- SDKPrinterModule/src/main/java/com/example/sdkprintermodule/iu/bottomsheet/bottomsheet/ActionBottomDialogFragment.kt
+++ SDKPrinterModule/src/main/java/com/example/sdkprintermodule/iu/bottomsheet/bottomsheet/ActionBottomDialogFragment.kt
@@ -2,7 +2,6 @@ package com.example.sdkprintermodule.iu.bottomsheet.bottomsheet
 
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,8 +9,6 @@ import android.view.ViewGroup
 import androidx.navigation.findNavController
 import com.example.sdkprintermodule.R
 import com.example.sdkprintermodule.databinding.BottomSheetDialogLayoutBinding
-import com.example.sdkprintermodule.iu.bottomsheet.bixolon.PrinterControl.BixolonPrinter
-import com.example.sdkprintermodule.iu.bottomsheet.bottomsheet.ActionBottomDialogFragment.Companion.bxlPrinter
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class ActionBottomDialogFragment : BottomSheetDialogFragment() {
@@ -19,7 +16,7 @@ class ActionBottomDialogFragment : BottomSheetDialogFragment() {
     lateinit var mBinding: BottomSheetDialogLayoutBinding
 
     companion object {
-        var bxlPrinter: BixolonPrinter? = null
+        //        var bxlPrinter: BixolonPrinter? = null
         val EXTRA_ADDRESS = "Device_address"
         const val TAG = "CustomBottomSheetDialogFragment"
         var m_bluetoothAdapter: BluetoothAdapter? = null
@@ -27,7 +24,11 @@ class ActionBottomDialogFragment : BottomSheetDialogFragment() {
         val REQUEST_ENABLE_BLUETOOTH = 1
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         mBinding = BottomSheetDialogLayoutBinding.inflate(layoutInflater)
         return mBinding.root
     }
@@ -35,7 +36,7 @@ class ActionBottomDialogFragment : BottomSheetDialogFragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        bxlPrinter = BixolonPrinter(context)
+//        bxlPrinter = BixolonPrinter(context)
 
         mBinding.apply {
             btnClose.setOnClickListener { dismiss() }
@@ -50,11 +51,11 @@ class ActionBottomDialogFragment : BottomSheetDialogFragment() {
                 when (findNavController().currentDestination!!.label) {
                     FragmentLabelEnum.CONNECT_FRAGMENT.titleLabel -> {
                         findNavController()
-                                .navigate(R.id.action_connectPrinterFragment_to_activateFragment)
+                            .navigate(R.id.action_connectPrinterFragment_to_activateFragment)
                     }
                     FragmentLabelEnum.TO_PRINT_FRAGMENT.titleLabel -> {
                         findNavController()
-                                .navigate(R.id.action_toPrintFragment_to_activateFragment)
+                            .navigate(R.id.action_toPrintFragment_to_activateFragment)
                     }
                     else -> {
                         findNavController().currentDestination!!.label
@@ -64,7 +65,7 @@ class ActionBottomDialogFragment : BottomSheetDialogFragment() {
                 when (findNavController().currentDestination!!.label) {
                     FragmentLabelEnum.ACTIVATE_FRAGMENT.titleLabel -> {
                         findNavController()
-                                .navigate(R.id.action_activateFragment_to_connectPrinterFragment)
+                            .navigate(R.id.action_activateFragment_to_connectPrinterFragment)
                     }
                     else -> {
                         findNavController().currentDestination!!.label
@@ -77,11 +78,11 @@ class ActionBottomDialogFragment : BottomSheetDialogFragment() {
     //todo revisar la instancia
     override fun onDestroy() {
         super.onDestroy()
-        bxlPrinter?.printerClose()
+//        bxlPrinter?.printerClose()
     }
 
 }
 
-fun BixolonPrinter.getPrinterInstance(): BixolonPrinter? {
-    return bxlPrinter
-}
+//fun BixolonPrinter.getPrinterInstance(): BixolonPrinter? {
+//    return bxlPrinter
+//}
