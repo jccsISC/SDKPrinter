6835ccd676b042c48d71202d8c6eefd798ce87ba
jccsISC <silva.jccs18@gmail.com>
2021-07-04 23:17:26 -0500

    Creando el modulo para pasarlo a kotlin

---
+++ SDKPrinterModule/src/main/java/com/example/sdkprintermodule/iu/bottomsheet/ActionBottomDialogFragment.kt
@@ -0,0 +1,8 @@
+package com.example.sdkprintermodule.iu.bottomsheet
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class ActionBottomDialogFragment : BottomSheetDialogFragment() {
+
+
+}
